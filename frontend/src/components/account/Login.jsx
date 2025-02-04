0e1ada19cc8f89de57487908b2da86e8a70fba15
Sahith-Rao <sahithraolingampally@gmail.com>
2025-02-04 22:00:19 +0530

    login/signup page created

--- frontend/src/components/account/Login.jsx
+++ frontend/src/components/account/Login.jsx
@@ -1,4 +1,5 @@
-import { Box,TextField,Button,styled } from '@mui/material';
+import { useState } from 'react';
+import { Box,TextField,Button,styled,Typography } from '@mui/material';
 
 const Component = styled(Box)`
     width: 400px;
@@ -8,19 +9,72 @@ const Component = styled(Box)`
 const Image = styled(`img`)({
     width: 100,
     margin: 'auto',
-    display: 'flex'
+    display: 'flex',
+    padding: '50px 0 0'
 
 })
+
+const Wrapper = styled(Box)`
+    padding: 25px 35px;
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    & > div, & > button, & > p {
+        margin-top: 20px;
+    }
+`;
+
+const LoginButton = styled(Button)`
+    text-transform: none;
+    height: 48px;
+    border-radius: 2px;
+`;
+
+const SignupButton = styled(Button)`
+    text-transform: none;
+    height: 48px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`;
+
+const Text = styled(Typography)`
+    color: #878787;
+    font-size: 16px;
+`;
+
+
 const Login = () => {
     const imageURL = 'https://cdn-icons-png.flaticon.com/512/10026/10026257.png';
-
+    const [account,toggleAccount] = useState('login');
+    const toggleSignup = () => {
+        account === 'signup' ? toggleAccount('login') : toggleAccount('signup');
+    }
     return (
         <Component>
-            <Image src = {imageURL} alt = "login" />
-            <TextField variant='filled'/>
-            <TextField variant='filled'/>
-            <Button variant='contained'>Login</Button>
-            <Button variant='outlined'>Create an account</Button>
+            <Box>
+                <Image src = {imageURL} alt = "login" />
+                {
+                    account === 'login' ?
+                        <Wrapper>
+                            <TextField variant='filled' label = "Enter Username"/>
+                            <TextField variant='filled' label = "Enter Password"/>
+                            <LoginButton variant='contained'>Login</LoginButton>
+                            <Text>OR</Text>
+                            <SignupButton variant='outlined' onClick={() => toggleSignup()}>Create an account</SignupButton>
+                        </Wrapper> 
+                    :
+                        <Wrapper>
+                            <TextField variant='filled' label = "Enter Name"/>
+                            <TextField variant='filled' label = "Enter Username"/>
+                            <TextField variant='filled' label = "Enter Password"/>
+                            <SignupButton variant='contained'>Signup</SignupButton>
+                            <Text>OR</Text>
+                            <LoginButton variant='outlined' onClick={() => toggleSignup()}>Already have an account?</LoginButton>
+                        </Wrapper>
+}
+            </Box>
+            
         </Component>
     )
 }
