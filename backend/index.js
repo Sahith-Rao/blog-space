756f93fe0ac93e9954e4159fba07a78dd30eb20d
Sahith-Rao <sahithraolingampally@gmail.com>
2025-02-09 19:14:04 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import Connection from './database/db.js';
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 8000;
+const PORT = 8080;
 
 app.listen(PORT, ()=> console.log("Sever is hi running"));
+
+const USERNAME = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
+
+Connection(USERNAME,PASSWORD);
