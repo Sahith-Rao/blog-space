3137842fded2dfb4aead9d2a403fbd14b22ac2a4
Sahith-Rao <sahithraolingampally@gmail.com>
2025-02-25 00:33:55 +0530

    vercel 11

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,23 +1,23 @@
-const multer = require('multer');
+const { v2: cloudinary } = require('cloudinary');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('cloudinary').v2;
+const multer = require('multer');
 
-// Configure Cloudinary
+// Configure Cloudinary with environment variables
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
-// Set up multer to use Cloudinary storage
+// Set up storage for multer
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'blogit_posts', // Folder name in Cloudinary
-    allowed_formats: ['jpg', 'png', 'jpeg'],
+    folder: 'blogit_uploads', // Folder name in Cloudinary
+    allowed_formats: ['jpg', 'png', 'jpeg', 'gif'],
   },
 });
 
-const uploadMiddleware = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
-module.exports = uploadMiddleware;
+module.exports = upload;
